9f9ce95bd751a5760f06ddb2d3055e7a92092d4a
OmkarLande <omkarrlande.534@gmail.com>
2024-01-26 17:59:08 +0530

    color correction

--- components/FormSubmitComponent.tsx
+++ components/FormSubmitComponent.tsx
@@ -66,7 +66,7 @@ function FormSubmitComponent({ formUrl, content }: { content: FormElementInstanc
   if (submitted) {
     return (
       <div className="flex justify-center w-full h-full items-center p-8">
-        <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded">
+        <div className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-rose-700 rounded">
           <h1 className="text-2xl font-bold">Form submitted</h1>
           <p className="text-muted-foreground">Thank you for submitting the form, you can close this page now.</p>
         </div>
@@ -78,7 +78,7 @@ function FormSubmitComponent({ formUrl, content }: { content: FormElementInstanc
     <div className="flex justify-center w-full h-full items-center p-8">
       <div
         key={renderKey}
-        className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-blue-700 rounded"
+        className="max-w-[620px] flex flex-col gap-4 flex-grow bg-background w-full p-8 overflow-y-auto border shadow-xl shadow-rose-700 rounded"
       >
         {content.map((element) => {
           const FormElement = FormElements[element.type].formComponent;
